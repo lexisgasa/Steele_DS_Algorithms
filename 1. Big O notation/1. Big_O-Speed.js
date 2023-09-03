ec01ad71c63d0ca7e38813f70adafdb979b4c259
Alexis <lexisgasa3007@gmail.com>
2023-09-03 22:41:49 +0200

    big O speed test

--- 1. Big O notation/1. Big_O-Speed.js
+++ 1. Big O notation/1. Big_O-Speed.js
@@ -13,3 +13,16 @@ function add1 (n) {
 function add2 (n) {
     return n * (n + 1) / 2;
 };
+
+// guardamos en variable el tiempo antes y despues de llamar a la funcion, el metodo performance.now() da un resultado en milisegundos, por lo que se divide / 1000 para obtener los segundos que tarda en ejecutarse el algoritmo.
+let time1_1 = performance.now();
+add1(10000000);
+let time2_1 = performance.now();
+console.log(`Time Elapsed: ${(time2_1 - time1_1) / 1000} seconds.`)
+
+let time1_2 = performance.now();
+add2(10000000);
+let time2_2 = performance.now();
+console.log(`Time Elapsed: ${(time2_2 - time1_2) / 1000} seconds.`)
+
+// el problema con mirar el tiempo es que en cada pc se obtendran valores totalmente diferentes, y que de hecho en el mismo pc se VAN a obtener resultados diferentes
